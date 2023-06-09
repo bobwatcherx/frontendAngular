a39a1288ed45ed9e8ee1f72e035e26514a7af243
root <bobwatcherx@gmail.com>
2023-06-09 11:09:16 +0700

    fx

--- src/app/api/api-service.service.ts
+++ src/app/api/api-service.service.ts
@@ -9,26 +9,26 @@ import { environment } from 'src/environments/environment';
 export class ApiServiceService {
   tokenkey: any;
   books: any[] = [];
-
+  apiUrl = "https://book-store-bobwatcherx.vercel.app/api"
   constructor(
     private http: HttpClient
   ) { }
 public addProduct(productData: any): Observable<any> {
-    return this.http.post<any>(`${environment.ApiURL}/api/create/product`, productData);
+    return this.http.post<any>(`https://book-store-bobwatcherx.vercel.app/api/api/create/product`, productData);
   }
  public deleteBook(bookId: string): Observable<any> {
-    const url = `${environment.ApiURL}/api/delete/products/${bookId}`;
+    const url = `https://book-store-bobwatcherx.vercel.app/api/api/delete/products/${bookId}`;
     return this.http.delete<any>(url);
   }
 
  public updateProduct(id: number, product: any): Observable<any> {
-    const url = `${environment.ApiURL}/api/products/${id}`;
+    const url = `https://book-store-bobwatcherx.vercel.app/api/api/products/${id}`;
     return this.http.put(url, product);
   }
 
 
  public getHistoryBayar(): Observable<any> {
-    return this.http.get<any>(environment.ApiURL + '/api/historybayar');
+    return this.http.get<any>('https://book-store-bobwatcherx.vercel.app/api/api/historybayar');
   }
   // public register(form: any) {
   //   return this.http.post(
@@ -41,16 +41,16 @@ public addProduct(productData: any): Observable<any> {
   //   );
   // }
   register(formData: any): Observable<any> {
-    return this.http.post(environment.ApiURL + '/api/register', formData, { responseType: 'json' });
+    return this.http.post('https://book-store-bobwatcherx.vercel.app/api/api/register', formData, { responseType: 'json' });
   }
    getProductById(id: number): Observable<any> {
-    const url = `${environment.ApiURL}/api/search/products/${id}`;
+    const url = `https://book-store-bobwatcherx.vercel.app/api/api/search/products/${id}`;
     return this.http.get<any>(url);
   }
 
    public fixbayar(form: any) {
     return this.http.post(
-      environment.ApiURL + '/api/bayar',
+      'https://book-store-bobwatcherx.vercel.app/api/api/bayar',
       {
         nama_barang: form.nama_barang,
         email: form.email,
@@ -62,7 +62,7 @@ public addProduct(productData: any): Observable<any> {
   }
   Login(form: any) {
     return this.http.post(
-      environment.ApiURL + '/api/login',
+      'https://book-store-bobwatcherx.vercel.app/api/api/login',
       {
         email: form.email,
         password: form.password,
@@ -72,7 +72,7 @@ public addProduct(productData: any): Observable<any> {
   }
 
  GetListBooks() {
-  return this.http.get(environment.ApiURL + '/api/products');
+  return this.http.get('https://book-store-bobwatcherx.vercel.app/api/api/products');
 }
 
 
@@ -85,14 +85,14 @@ public addProduct(productData: any): Observable<any> {
     
 
     return this.http.post<any>(
-      environment.ApiURL + '/api/admin/products',
+      'https://book-store-bobwatcherx.vercel.app/api/api/admin/products',
       form,
       { headers:headers }
     );
   }
   
   UpdateBooks( id : any, form : any){
-   return this.http.post( environment.ApiURL + '/api/admin/products',
+   return this.http.post('https://book-store-bobwatcherx.vercel.app/api/api/admin/products',
    {
       "judul" : 'judul',
       "author" : 'author',
@@ -108,7 +108,7 @@ public addProduct(productData: any): Observable<any> {
   }
 
   public DeleteBooks(id :any) {
-    return this.http.delete(environment.ApiURL + '/api/admin/delete-products/20');
+    return this.http.delete('https://book-store-bobwatcherx.vercel.app/api/api/admin/delete-products/20');
   }
   }
 
