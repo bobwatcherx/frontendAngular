d073c2fc55a7e7c913d78b8d00facb9829b1cf28
root <bobwatcherx@gmail.com>
2023-06-09 11:01:23 +0700

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   ApiURL : 'https://book-store-bobwatcherx.vercel.app/api'
   // ApiURL : 'http://localhost:8000'
 };
