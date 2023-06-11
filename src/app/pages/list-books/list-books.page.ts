a5b7d13e9be84052fba631cb0252b51f3b492c5f
root <bobwatcherx@gmail.com>
2023-06-11 16:57:18 +0700

    perbaiki counter

--- src/app/pages/list-books/list-books.page.ts
+++ src/app/pages/list-books/list-books.page.ts
@@ -2,21 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { ApiServiceService } from 'src/app/api/api-service.service';
 
+interface Book {
+  judul: string;
+  harga: number;
+  gambar: string;
+  sinopsis: string;
+  counter: number;
+}
+
 @Component({
   selector: 'app-list-books',
   templateUrl: './list-books.page.html',
   styleUrls: ['./list-books.page.scss'],
 })
 export class ListBooksPage implements OnInit {
-  counter: number = 1;
-  books: any[] = [];
+  books: Book[] = [];
 
   constructor(private api: ApiServiceService, private router: Router) { }
 
-  increment() {
-    this.counter++;
+  increment(book: Book) {
+    book.counter++;
   }
-logout() {
+
+  decrement(book: Book) {
+    if (book.counter > 1) {
+      book.counter--;
+    }
+  }
+
+  logout() {
     // Menghapus data dari local storage
     localStorage.removeItem('email');
     localStorage.removeItem('token');
@@ -25,36 +39,32 @@ logout() {
     // Melakukan pengalihan halaman ke '/login' dengan replaceUrl true
     this.router.navigateByUrl('/login', { replaceUrl: true });
   }
-  decrement() {
-    if (this.counter > 1) {
-      this.counter--;
-    }
+
+  doCheckout(book: Book) {
+    const { judul, harga, gambar, sinopsis } = book;
+    const subtotal = book.counter * harga;
+    const discount = subtotal * 0.1;
+    const grandtotal = (subtotal - discount) - 10;
+
+    const navigationExtras: NavigationExtras = {
+      state: {
+        counter: book.counter,
+        judul: judul,
+        price: harga,
+        gambar: gambar,
+        sinopsis: sinopsis,
+        grandtotal: grandtotal
+      }
+    };
+
+    this.router.navigateByUrl('checkout', navigationExtras);
   }
 
-  doCheckout(judul: string, price: number,gambar:string,sinopsis:string) {
-  const subtotal = this.counter * price;
-  const discount = subtotal * 0.1;
-  const grandtotal = (subtotal - discount) - 10;
-
-  const navigationExtras: NavigationExtras = {
-    state: {
-      counter: this.counter,
-       judul: judul,
-      price: price,
-      gambar:gambar,
-      sinopsis:sinopsis,
-      grandtotal:grandtotal
-    }
-  };
-
-  this.router.navigateByUrl('checkout', navigationExtras);
-}
-
-  detail(book: any) {
-  // Akses detail buku dengan menggunakan variabel 'book'
-  console.log(book);
-  this.router.navigateByUrl('detail');
-}
+  detail(book: Book) {
+    // Akses detail buku dengan menggunakan variabel 'book'
+    console.log(book);
+    this.router.navigateByUrl('detail');
+  }
 
   ngOnInit() {
     this.getListBooks();
@@ -62,7 +72,10 @@ logout() {
 
   getListBooks() {
     this.api.GetListBooks().subscribe((res: any) => {
-      this.books = res;
+      this.books = res.map((bookData: any) => ({
+        ...bookData,
+        counter: 1
+      }));
       console.log(res);
     });
   }
